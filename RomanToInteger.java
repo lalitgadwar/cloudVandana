1787b6b8ef2cd4b90ddc40ce817600c8acd4f06a
Lalit Gadwar <148062161+lalitgadwar@users.noreply.github.com>
2023-10-19 14:38:12 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,44 @@
+package cloudVandana;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+public class RomanToInteger {
+    public static void main(String[] args) {
+    	Scanner sc= new Scanner(System.in);
+    	System.out.println("please Enter Roman Number(in capital letter)");
+        String romanNumeral = sc.next();
+
+        int result = romanToInteger(romanNumeral);
+        System.out.println("The integer value of " + romanNumeral + " is: " + result);
+    }
+
+    public static int romanToInteger(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanMap.get(s.charAt(i));
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+}
+
